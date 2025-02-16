2e4c969f13d68af5afefb1ad0b8278a5c98ff590
obuzietter <obuyacalvince672@gmail.com>
2025-02-16 22:05:42 +0300

    Refactors invoices table structure
    Updates the invoices table structure to use foreign key constraints for the customer_id field and changes the data type of several fields to decimal. This improves data integrity and allows for more precise calculations.

--- database/migrations/2025_02_07_201424_create_invoices_table.php
+++ database/migrations/2025_02_07_201424_create_invoices_table.php
@@ -14,18 +14,18 @@ return new class extends Migration
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
             $table->string('invoice_number', 60)->unique();
-            $table->string('customer_id')->nullable();
+            $table->foreignId('customer_id')->constrained('customers')->onDelete('restrict');
             $table->string('company_id')->nullable();
             $table->unsignedBigInteger('payment_id')->nullable();
-            $table->unsignedDecimal('sub_total', 15, 2);
-            $table->unsignedDecimal('tax_amount', 15, 2)->default(0.00);
-            $table->unsignedDecimal('shipping_amount', 15, 2)->default(0.00);
-            $table->unsignedDecimal('discount_amount', 15, 2)->default(0.00);
+            $table->decimal('sub_total', 15, 2)->unsigned();
+            $table->decimal('tax_amount', 15, 2)->unsigned()->default(0.00);
+            $table->decimal('shipping_amount', 15, 2)->unsigned()->default(0.00);
+            $table->decimal('discount_amount', 15, 2)->unsigned()->default(0.00);
             $table->string('shipping_option', 60)->nullable();
             $table->string('shipping_method', 60)->nullable();
             $table->string('coupon_code', 120)->nullable();
             $table->string('discount_description')->nullable();
-            $table->unsignedDecimal('amount', 15, 2);
+            $table->decimal('amount', 15, 2)->unsigned();
             $table->enum('payment_status', ['pending', 'paid', 'partial', 'refunded', 'canceled', 'overdue'])->default('pending');
             $table->timestamp('paid_at')->nullable();
             $table->timestamps();
