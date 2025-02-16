fc2367d964db883f8c876b72e82d4012f670cfdb
obuzietter <obuyacalvince672@gmail.com>
2025-02-16 23:12:38 +0300

    Create payments table with UUID primary key and additional fields for payment processing

--- database/migrations/2025_02_16_193333_create_payments_table.php
+++ database/migrations/2025_02_16_193333_create_payments_table.php
@@ -12,7 +12,22 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('payments', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary(); // Using UUID as primary key
+            $table->string('invoice_id')->nullable();    
+            $table->enum('state', ['PENDING', 'PROCESSING', 'FAILED', 'CANCELED', 'PARTIAL', 'COMPLETE', 'RETRY'])->default('PENDING');            
+            $table->enum('provider', ['M-PESA', 'CARD-PAYMENT', 'BITCOIN', 'BANK-ACH', 'COOP_B2B']);
+            $table->decimal('charges', 10, 2)->default(0.00);
+            $table->decimal('net_amount', 10, 2);
+            $table->string('currency', 3)->default('KES');
+            $table->decimal('value', 10, 2);
+            $table->string('account');
+            $table->string('api_ref')->nullable();
+            $table->string('mpesa_reference')->nullable();
+            $table->text('failed_reason')->nullable();
+            $table->string('failed_code')->nullable();
+            $table->foreignId('customer_id')->nullable()->constrained('customers')->onDelete('set null');
+            // $table->foreignId('customer_id')->nullable()->constrained('customers')->nullOnDelete();
+            $table->string('phone_number');
             $table->timestamps();
         });
     }
