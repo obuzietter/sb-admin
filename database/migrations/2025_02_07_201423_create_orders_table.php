a76aaad7110df5589df17b90e196a49fce993fdf
obuzietter <obuyacalvince672@gmail.com>
2025-02-17 22:02:38 +0300

    Adds user_id and payment_id foreign keys to orders table
    Updates the orders table migration to include foreign key constraints for user_id and payment_id columns. Also adds additional columns for order status, shipping options, tax amount, shipping amount, description, coupon code, discount amount, sub total, amount, confirmation status, discount description, cancellation reason, and token.
    
    Relates to #123

--- database/migrations/2025_02_07_201423_create_orders_table.php
+++ database/migrations/2025_02_07_201423_create_orders_table.php
@@ -13,6 +13,25 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->uuid('payment_id')->nullable();
+            $table->foreign('payment_id')->references('id')->on('payments')->nullOnDelete();
+            $table->enum('status', ['pending', 'processing', 'completed', 'cancelled'])->default('pending');
+            $table->string('shipping_option', 60)->nullable();
+            $table->string('shipping_method', 60)->default('default');
+            $table->decimal('tax_amount', 15, 2)->nullable();
+            $table->decimal('shipping_amount', 15, 2)->nullable();
+            $table->text('description')->nullable();
+            $table->string('coupon_code', 120)->nullable();
+            $table->decimal('discount_amount', 15, 2)->nullable();
+            $table->decimal('sub_total', 15, 2);
+            $table->decimal('amount', 15, 2);
+            $table->boolean('is_confirmed')->default(false);
+            $table->string('discount_description', 191)->nullable();
+            $table->string('cancellation_reason', 191)->nullable();
+            $table->string('token', 120)->nullable();
+            
+
             $table->timestamps();
         });
     }
