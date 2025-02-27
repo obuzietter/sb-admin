03cfc165108dfde04293d087fe4c75e7dfe98845
obuzietter <obuyacalvince672@gmail.com>
2025-02-27 23:20:50 +0300

    Refactors head section for better maintainability
    Moves head links to a separate Blade component for cleaner and more maintainable code structure.

--- resources/views/shop/layouts/shop.blade.php
+++ resources/views/shop/layouts/shop.blade.php
@@ -3,32 +3,15 @@
 
     <head>
         <meta charset="utf-8">
-        {{-- <title>Fruitables - Vegetable Website Template</title> --}}
-        <title>@yield('title')</title>
+        
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <meta content="" name="keywords">
         <meta content="" name="description">
+        <title>@yield('title')</title>
 
-        <!-- Google Web Fonts -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Raleway:wght@600;800&display=swap" rel="stylesheet"> 
+        {{-- head links --}}
+        @include('shop.layouts.components.head-links')
 
-        <!-- Icon Font Stylesheet -->
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-
-        <!-- Libraries Stylesheet -->
-        <link href="/shop/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
-        <link href="/shop/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-
-
-        <!-- Customized Bootstrap Stylesheet -->
-        <!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-        <!-- Template Stylesheet -->
-        <link href="/shop/css/style.css" rel="stylesheet">
     </head>
 
     <body>
