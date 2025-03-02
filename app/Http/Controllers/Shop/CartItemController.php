5adf44450442fcb66b6d8bc9dce0116d417f5227
obuzietter <obuyacalvince672@gmail.com>
2025-03-02 23:09:23 +0300

    Renames CartController to CartItemController

--- app/Http/Controllers/Shop/CartItemController.php
+++ app/Http/Controllers/Shop/CartItemController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Shop;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class CartController extends Controller
+class CartItemController extends Controller
 {
     /**
      * Display a listing of the resource.
