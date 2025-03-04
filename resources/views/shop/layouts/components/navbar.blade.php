4eb1432f35bc3778fcfda430de45d5d64f0cf341
obuzietter <obuyacalvince672@gmail.com>
2025-03-04 22:15:36 +0300

    Updates navbar brand link to use route helper for improved maintainability

--- resources/views/shop/layouts/components/navbar.blade.php
+++ resources/views/shop/layouts/components/navbar.blade.php
@@ -16,7 +16,7 @@
     </div>
     <div class="container px-0">
         <nav class="navbar navbar-light bg-white navbar-expand-xl">
-            <a href="index.html" class="navbar-brand">
+            <a href="{{route('home')}}" class="navbar-brand">
                 <h1 class="text-primary display-6 fw-bold">MDC</h1>
             </a>
             <button class="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse"
