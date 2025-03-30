a6639529c097fce4db0f6fdad90bf43b133921a6
obuzietter <obuyacalvince672@gmail.com>
2025-03-30 22:20:17 +0300

    Contact page modified

--- resources/views/shop/contact.blade.php
+++ resources/views/shop/contact.blade.php
@@ -23,16 +23,14 @@
                     <div class="col-12">
                         <div class="text-center mx-auto" style="max-width: 700px;">
                             <h1 class="text-primary">Get in touch</h1>
-                            <p class="mb-4">The contact form is currently inactive. Get a functional and working contact
-                                form with Ajax & PHP in a few minutes. Just copy and paste the files, add a little code and
-                                you're done. <a href="https://htmlcodex.com/contact-form">Download Now</a>.</p>
+                            <p class="mb-4">
+                                Feel free to reach out to us for any inquiries, feedback, or assistance. We are here to help and ensure you have the best experience possible.
+                            </p>
                         </div>
                     </div>
                     <div class="col-lg-12">
                         <div class="h-100 rounded">
-                            <iframe class="rounded w-100" style="height: 400px;"
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.9145059124753!2d36.885849474724495!3d-1.219576098768783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f3fc955678a67%3A0x2c30e07746ac972a!2sTRM%20-%20Thika%20Road%20Mall!5e0!3m2!1sen!2ske!4v1740693388004!5m2!1sen!2ske" loading="lazy"
-                                referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1406.6016481081444!2d34.60223903596572!3d-0.7569982496400229!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182b4987f9dbf7df%3A0xd53278191eb68591!2sMaganda%20Discount%20Center%20Supermarket!5e0!3m2!1sen!2ske!4v1743361888737!5m2!1sen!2ske" class="rounded w-100" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </div>
                     </div>
                     <div class="col-lg-7">
@@ -50,21 +48,21 @@
                             <i class="fas fa-map-marker-alt fa-2x text-primary me-4"></i>
                             <div>
                                 <h4>Address</h4>
-                                <p class="mb-2">123 Street New York.USA</p>
+                                <a href="https://maps.app.goo.gl/d3xgt1DkE3Y3cesx5" target="_blank"><p class="mb-2">Weighbridge, Kisii-Migori Highway</p></a>
                             </div>
                         </div>
                         <div class="d-flex p-4 rounded mb-4 bg-white">
                             <i class="fas fa-envelope fa-2x text-primary me-4"></i>
                             <div>
                                 <h4>Mail Us</h4>
-                                <p class="mb-2">info@example.com</p>
+                                <a href="mailto:magandadiscountcentre@yahoo.com"><p class="mb-2">magandadiscountcentre@yahoo.com</p></a>
                             </div>
                         </div>
                         <div class="d-flex p-4 rounded bg-white">
                             <i class="fa fa-phone-alt fa-2x text-primary me-4"></i>
                             <div>
                                 <h4>Telephone</h4>
-                                <p class="mb-2">(+012) 3456 7890</p>
+                                <a href="tel:+254722237347"><p class="mb-2">(+254) 7222 37347</p></a>
                             </div>
                         </div>
                     </div>
